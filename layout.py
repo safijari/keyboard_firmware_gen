3dd8b6b93b95be2418b3d9d24f24f7bf04894ae6
Jariullah <safijari@isu.edu>
2021-02-08 07:10:51 -0800

    halfway through

--- layout.py
+++ layout.py
@@ -72,26 +72,30 @@ KEY_F23
 KEY_F24
 """
 
+# mod_locations =
+# (6, 6) for ctrl/alt, no mod shift
+# ()
+
 layout_left = {
-    1: {1: "=", 2: "1", 3: "2", 4: "3", 5: "4", 6: "5",},
-    2: {1: "LAYER", 2: "q", 3: "w", 4: "e", 5: "r", 6: "t",},
+    1: {1: "NO_OP", 2: "NO_OP", 3: "NO_OP", 4: "NO_OP", 5: "NO_OP", 6: "NO_OP",},
+    2: {1: "KEY_ESC", 2: "q", 3: "w", 4: "e", 5: "r", 6: "t",},
     3: {1: ["KEY_LEFT_GUI", "KEY_TAB"], 2: "a", 3: "s", 4: "d", 5: "f", 6: "g",},
     4: {1: "KEY_LEFT_SHIFT", 2: "z", 3: "x", 4: "c", 5: "v", 6: "b",},
-    5: {1: "LAYER", 2: "`", 3: "MOUSE_MIDDLE", 4: "MOUSE_LEFT", 5: "MOUSE_RIGHT",},
-    6: {5: "KEY_BACKSPACE", 6: "KEY_LEFT_CTRL",},
-    7: {5: ["LAYER", "KEY_ESC"], 6: "LAYER",},
-    8: {5: "LAYER", 6: "KEY_LEFT_ALT",},
+    5: {1: "NO_OP", 2: "NO_OP" "NO_OP", 4: "NO_OP", 5: "NO_OP",},
+    6: {5: "NO_OP", 6: "KEY_LEFT_CTRL",},
+    7: {5: "NO_OP", 6: "NO_OP",},
+    8: {5: "NO_OP", 6: "NO_OP",},
 }
 
 layout_right = {
-    1: {1: "-", 2: "0", 3: "9", 4: "8", 5: "7", 6: "6",},
-    2: {1: ["LAYER", "\\"], 2: "p", 3: "o", 4: "i", 5: "u", 6: "y",},
+    1: {1: "NO_OP", 2: "NO_OP", 3: "NO_OP", 4: "NO_OP", 5: "NO_OP", 6: "NO_OP",},
+    2: {1: "KEY_BACKSPACE", 2: "p", 3: "o", 4: "i", 5: "u", 6: "y",},
     3: {1: "'", 2: ";", 3: "l", 4: "k", 5: "j", 6: "h",},
-    4: {1: "KEY_RIGHT_SHIFT", 2: "/", 3: ".", 4: ",", 5: "m", 6: "n",},
-    5: {1: "LAYER", 2: "]", 3: "[", 4: "NO_OP", 5: "NO_OP",},
-    6: {5: " ", 6: "KEY_RIGHT_CTRL",},
-    7: {5: "KEY_RETURN", 6: "KEY_RIGHT_GUI",},
-    8: {5: "LAYER", 6: "KEY_RIGHT_ALT",},
+    4: {1: ["KEY_RIGHT_SHIFT", "KEY_RETURN"], 2: "/", 3: ".", 4: ",", 5: "m", 6: "n",},
+    5: {1: "NO_OP", 2: "NO_OP" "NO_OP", 4: "NO_OP", 5: "NO_OP",},
+    6: {5: "NO_OP", 6: "KEY_LEFT_ALT",},
+    7: {5: "NO_OP", 6: "NO_OP",},
+    8: {5: "NO_OP", 6: "NO_OP",},
 }
 
 layers = {
